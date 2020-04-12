a53cb1748b6b718ff5eb66472d6bb58912b186f1
Matthew LaRocca <mlarocca.cpp@gmail.com>
2020-04-12 07:40:10 -0700

    Draw mark with correct note width

--- Scripts/NoteCreator.cs
+++ Scripts/NoteCreator.cs
@@ -25,11 +25,6 @@ namespace ABCUnity
             Default, Down, Up
         }
 
-        enum StaffMarker
-        {
-            None, Middle, Above, Below
-        }
-
         /// <summary> The distance between note values on the staff. </summary>
         const float noteStep = 0.28f;
 
@@ -44,24 +39,29 @@ namespace ABCUnity
         /// <summary>
         /// Draws all required staff markers for a note  with a given stepcount from the Staff's zero value.
         /// </summary>
-        bool AddNoteStaffMarkers(int stepCount, GameObject container, Vector3 offset)
+        private bool AddNoteStaffMarkers(int stepCount, GameObject container, Vector3 offset, float localScaleX)
         {
             if (stepCount < -2)  // below the staff
             {
                 int stepOffset = stepCount % 2 == 0 ? 1 : 0;
-                for (int sc = stepCount + 2 + stepOffset; sc < -2; sc += 2)
-                    CreateStaffMark(sc, container, offset);
 
-                CreateStaffMark(stepCount + stepOffset, container, offset);
+                // if step offset is zero then the mark is though the middle of the note
+                CreateStaffMark(stepCount + stepOffset, container, offset, stepOffset == 0 ? 1.0f : localScaleX);
+
+                for (int sc = stepCount + 2 + stepOffset; sc < -2; sc += 2)
+                    CreateStaffMark(sc, container, offset, localScaleX);
 
                 return true;
             }
             else if (stepCount > 8) // above the staff
             {
+                if (stepCount % 2 == 0) // mark belongs above
+                    CreateStaffMark(stepCount - 1, container, offset, localScaleX);
+                else // mark in middle
+                    CreateStaffMark(stepCount, container, offset, 1.0f);
+
                 for (int sc = stepCount - 2; sc > 8; sc -= 2)
-                    CreateStaffMark(sc, container, offset);
-                
-                CreateStaffMark(stepCount % 2 == 0 ? stepCount - 1 : stepCount, container, offset);
+                    CreateStaffMark(sc, container, offset, localScaleX);
 
                 return true;
             }
@@ -74,7 +74,7 @@ namespace ABCUnity
             int stepCount = note.value - clefZero[clef];
 
             var noteDirection = stepCount > 3 ? NoteDirection.Down : NoteDirection.Up;
-            bool addedMarkers = AddNoteStaffMarkers(stepCount, container, offset);
+            bool addedMarkers = AddNoteStaffMarkers(stepCount, container, offset, 1.0f);
 
             var notePosition = offset + new Vector3(0.0f, noteStep * stepCount, 0.0f);
 
@@ -100,7 +100,7 @@ namespace ABCUnity
             return noteObj;
         }
 
-        SpriteRenderer AddChordDot(ABC.Note note, ABC.Clef clef, NoteDirection noteDirection, GameObject container, Vector3 offset)
+        private SpriteRenderer AddChordDot(ABC.Note note, ABC.Clef clef, NoteDirection noteDirection, GameObject container, Vector3 offset)
         {
             int stepCount = note.value - clefZero[clef];
 
@@ -119,7 +119,7 @@ namespace ABCUnity
         /// The direction is chosen by examining the extreme notes of the chord and picking the direction based on the note which is farthest away from the value of the middle staffline.
         /// Precondition: notes array should be sorted in ascending order.
         /// </summary>
-        NoteDirection DetermineChordNoteDirection(ABC.Note[] sortedNotes, ABC.Clef clef)
+        private NoteDirection DetermineChordNoteDirection(ABC.Note[] sortedNotes, ABC.Clef clef)
         {
             var direction = NoteDirection.Down;
 
@@ -152,17 +152,25 @@ namespace ABCUnity
             Array.Sort(sortedNotes);
 
             var noteDirection = DetermineChordNoteDirection(sortedNotes, clef);
+            float staffMarkerScale = 1.0f;
 
-            bool hasMarkers = AddNoteStaffMarkers(notes[0].value - clefZero[clef], container, offset) || 
-                AddNoteStaffMarkers(notes[notes.Length - 1].value - clefZero[clef], container, offset);
+
+            if (ChordHasDots(notes))
+            {
+                staffMarkerScale = 2.0f;
+
+                // note that when the stem direction is down then the dot will be placed too close to the previous note.
+                // In this case we will push the chord over such that its min x value lines up with the caret.
+                if (noteDirection == NoteDirection.Down)
+                    offset = offset + new Vector3(chordDotOffset, 0.0f, 0.0f);
+            } 
+
+            // TODO: if both below only need to do lowest
+            bool hasMarkers = AddNoteStaffMarkers(notes[0].value - clefZero[clef], container, offset, staffMarkerScale) || 
+                AddNoteStaffMarkers(notes[notes.Length - 1].value - clefZero[clef], container, offset, staffMarkerScale);
 
             if (hasMarkers)
                 offset = offset + new Vector3(notePadding, 0.0f, 0.0f);
-
-            // note that when the stem direction is down then the dot will be placed too close to the previous note.
-            // In this case we will push the chord over such that its minx lines up with the caret.
-            if (noteDirection == NoteDirection.Down && ChordHasDots(notes))
-                offset = offset + new Vector3(chordDotOffset, 0.0f, 0.0f);
 
             var items = new List<SpriteRenderer>();
 
@@ -179,11 +187,12 @@ namespace ABCUnity
             return items;
         }
 
-        SpriteRenderer CreateStaffMark(int stepCount, GameObject container, Vector3 offset)
+        private SpriteRenderer CreateStaffMark(int stepCount, GameObject container, Vector3 offset, float localScaleX)
         {
             var mark = spriteCache.GetSpriteObject("Staff_Mark");
             mark.transform.parent = container.transform;
             mark.transform.localPosition = offset + new Vector3(0.0f, noteStep * stepCount, 0.0f);
+            mark.transform.localScale = new Vector3(localScaleX, 1.0f, 1.0f);
 
             return mark;
         }
