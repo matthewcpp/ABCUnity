f214eac2e0ec897ede782412acd4739ef2a834cd
Matthew LaRocca <mlarocca.cpp@gmail.com>
2020-04-12 07:38:03 -0700

    Correctly position note with mark

--- Scripts/NoteCreator.cs
+++ Scripts/NoteCreator.cs
@@ -30,9 +30,21 @@ namespace ABCUnity
             None, Middle, Above, Below
         }
 
+        /// <summary> The distance between note values on the staff. </summary>
         const float noteStep = 0.28f;
 
-        void ResolveStaffMarkers(int stepCount, GameObject container, Vector3 offset)
+        /// <summary> The distance to offset the Chord dot from its root note.  
+        /// If the stem direction is down this value will need to be negated. 
+        /// </summary>
+        const float chordDotOffset = 0.67f;
+
+        /// <summary> The distance to offset notes by if they have a mark.  This will ensure they are centered. </summary>
+        const float notePadding = 0.14f;
+
+        /// <summary>
+        /// Draws all required staff markers for a note  with a given stepcount from the Staff's zero value.
+        /// </summary>
+        bool AddNoteStaffMarkers(int stepCount, GameObject container, Vector3 offset)
         {
             if (stepCount < -2)  // below the staff
             {
@@ -41,42 +53,56 @@ namespace ABCUnity
                     CreateStaffMark(sc, container, offset);
 
                 CreateStaffMark(stepCount + stepOffset, container, offset);
-            }
 
+                return true;
+            }
             else if (stepCount > 8) // above the staff
             {
                 for (int sc = stepCount - 2; sc > 8; sc -= 2)
                     CreateStaffMark(sc, container, offset);
                 
                 CreateStaffMark(stepCount % 2 == 0 ? stepCount - 1 : stepCount, container, offset);
+
+                return true;
             }
+
+            return false;
         }
 
-        public SpriteRenderer CreateNote(ABC.Note note, ABC.Clef clef, GameObject container, Vector3 offset, NoteDirection directionOverride = NoteDirection.Default)
+        public SpriteRenderer CreateNote(ABC.Note note, ABC.Clef clef, GameObject container, Vector3 offset)
         {
             int stepCount = note.value - clefZero[clef];
 
-            var noteName = note.length.ToString();
-            var noteDirection = directionOverride;
-            ResolveStaffMarkers(stepCount, container, offset);
+            var noteDirection = stepCount > 3 ? NoteDirection.Down : NoteDirection.Up;
+            bool addedMarkers = AddNoteStaffMarkers(stepCount, container, offset);
 
-            if (noteDirection == NoteDirection.Default)
-                noteDirection = stepCount > 3 ? NoteDirection.Down : NoteDirection.Up;
+            var notePosition = offset + new Vector3(0.0f, noteStep * stepCount, 0.0f);
 
-            var noteObj = spriteCache.GetSpriteObject($"Note_{noteName}_{noteDirection.ToString()}_None");
+            if (addedMarkers)
+                notePosition = notePosition + new Vector3(notePadding, 0.0f, 0.0f);
+
+            var noteObj = spriteCache.GetSpriteObject($"Note_{note.length.ToString()}_{noteDirection.ToString()}_None");
             noteObj.transform.parent = container.transform;
-            noteObj.transform.localPosition = offset + new Vector3(0.0f, noteStep * stepCount, 0.0f);
+            noteObj.transform.localPosition = notePosition;
 
             return noteObj;
         }
 
-        const float chordDotOffset = 0.67f;
-        const float notePadding = 0.1f;
+        private SpriteRenderer AddChordNote(ABC.Note note, NoteDirection noteDirection, ABC.Clef clef, GameObject container, Vector3 offset)
+        {
+            int stepCount = note.value - clefZero[clef];
+            var notePosition = offset + new Vector3(0.0f, noteStep * stepCount, 0.0f);
+
+            var noteObj = spriteCache.GetSpriteObject($"Note_{note.length.ToString()}_{noteDirection.ToString()}_None");
+            noteObj.transform.parent = container.transform;
+            noteObj.transform.localPosition = notePosition;
+
+            return noteObj;
+        }
 
         SpriteRenderer AddChordDot(ABC.Note note, ABC.Clef clef, NoteDirection noteDirection, GameObject container, Vector3 offset)
         {
             int stepCount = note.value - clefZero[clef];
-            ResolveStaffMarkers(stepCount, container, offset);
 
             var notePos = new Vector3(noteDirection == NoteDirection.Up ? chordDotOffset : -chordDotOffset, noteStep * stepCount, 0.0f);
 
@@ -107,39 +133,47 @@ namespace ABCUnity
             return direction;
         }
 
+        private bool ChordHasDots(ABC.Note[] sortedNotes)
+        {
+            for (int i = 1; i < sortedNotes.Length; i++)
+            {
+                // If the note will not fit on the current line because there is a note right below it, then need to draw a chord dot
+                if (i % 2 == 1 && sortedNotes[i].value - sortedNotes[i - 1].value == 1)
+                    return true;
+            }
+
+            return false;
+        }
+
         public List<SpriteRenderer> CreateChord(ABC.Note[] notes, ABC.Clef clef, GameObject container, Vector3 offset)
         {
             var sortedNotes = new ABC.Note[notes.Length];
             Array.Copy(notes, sortedNotes, notes.Length);
             Array.Sort(sortedNotes);
 
-            var items = new List<SpriteRenderer>();
-
             var noteDirection = DetermineChordNoteDirection(sortedNotes, clef);
 
-            items.Add(CreateNote(sortedNotes[0], clef, container, offset, noteDirection));
+            bool hasMarkers = AddNoteStaffMarkers(notes[0].value - clefZero[clef], container, offset) || 
+                AddNoteStaffMarkers(notes[notes.Length - 1].value - clefZero[clef], container, offset);
 
-            bool needsAdjustment = false;
-            for (int i = 1; i < sortedNotes.Length; i++)
-            {
-                // If the note will not fit on the current line because there is a note right below it, then need to draw a chord dot
-                if (i % 2 == 1 && sortedNotes[i].value - sortedNotes[i-1].value == 1)
-                {
-                    items.Add(AddChordDot(sortedNotes[i], clef, noteDirection, container, offset));
-                    needsAdjustment = noteDirection == NoteDirection.Down;
-                }
-                else
-                {
-                    items.Add(CreateNote(sortedNotes[i], clef, container, offset, noteDirection));
-                }
-            }
+            if (hasMarkers)
+                offset = offset + new Vector3(notePadding, 0.0f, 0.0f);
 
             // note that when the stem direction is down then the dot will be placed too close to the previous note.
             // In this case we will push the chord over such that its minx lines up with the caret.
-            if (needsAdjustment)
+            if (noteDirection == NoteDirection.Down && ChordHasDots(notes))
+                offset = offset + new Vector3(chordDotOffset, 0.0f, 0.0f);
+
+            var items = new List<SpriteRenderer>();
+
+            items.Add(AddChordNote(sortedNotes[0], noteDirection, clef, container, offset));
+
+            for (int i = 1; i < sortedNotes.Length; i++)
             {
-                foreach (var item in items)
-                    item.transform.localPosition = item.transform.localPosition + new Vector3(chordDotOffset, 0.0f, 0.0f);
+                if (i % 2 == 1 && sortedNotes[i].value - sortedNotes[i-1].value == 1)
+                    items.Add(AddChordDot(sortedNotes[i], clef, noteDirection, container, offset));
+                else
+                    items.Add(AddChordNote(sortedNotes[i], noteDirection, clef, container, offset));
             }
             
             return items;
