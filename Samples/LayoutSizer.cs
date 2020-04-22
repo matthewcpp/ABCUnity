77ba0d0d5a9e6ceaa81d5b432df2a2a97fd18c92
Matthew LaRocca <mlarocca.cpp@gmail.com>
2020-04-21 23:17:59 -0700

    Update Sample

--- Samples/LayoutSizer.cs
+++ Samples/LayoutSizer.cs
@@ -2,7 +2,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.U2D;
+using TMPro;
+
 
 namespace ABCUnity.Example
 {
@@ -10,37 +11,49 @@ namespace ABCUnity.Example
     {
         [SerializeField] private Camera camera;
         [SerializeField] private Layout layout;
+        [SerializeField] private TextMeshPro title;
         
-        const float targetHeightScale = 0.80f;
-
         private float aspect;
         private float orthographicSize;
 
         public void Awake()
         {
-
             ResizeLayout();
+            layout.onLoaded += OnLoaded;
+        }
+
+        void OnLoaded(ABC.Tune tune)
+        {
+            title.text = tune.title;
         }
 
         public void Update()
         {
             if (aspect != camera.aspect || orthographicSize != camera.orthographicSize)
                 ResizeLayout();
+
+
+            if (Input.GetKeyDown(KeyCode.L))
+            {
+                Debug.Log(title.rectTransform.rect);
+            }
         }
 
         private void ResizeLayout()
         {
             aspect = camera.aspect;
             orthographicSize = camera.orthographicSize;
-            
+
             float orthoHeight = orthographicSize* 2.0f;
-            float targetHeight = orthoHeight * targetHeightScale;
             float targetWidth = orthoHeight * aspect;
 
-            var layoutCollider = layout.GetComponent<BoxCollider2D>();
-            layoutCollider.size = new Vector2(targetWidth, targetHeight);
-            
-            layout.transform.position = new Vector3(0.0f, (orthoHeight - targetHeight) / -2.0f, 0.0f);
+            var titleTransform = title.rectTransform;
+            titleTransform.position = new Vector3(0.0f, orthographicSize, 0.0f);
+            titleTransform.sizeDelta = new Vector2 (targetWidth, titleTransform.sizeDelta.y);
+
+            var layoutTransform = layout.GetComponent<RectTransform>();
+            layoutTransform.position = new Vector3(0.0f, titleTransform.position.y - titleTransform.rect.height, 0.0f);
+            layoutTransform.sizeDelta = new Vector2(targetWidth, orthoHeight - titleTransform.sizeDelta.y);
         }
     }
 
